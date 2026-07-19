9dc37e447f7d65f8dc61f7c5b084e5b0f3dd0953
fchautems <41154950+fchautems@users.noreply.github.com>
2026-07-20 00:49:01 +0200

    Ajoute le chargeur des résultats

---
+++ app-results.js
@@ -0,0 +1,6 @@
+document.write('<script src="scripts/app-results-1.js"><\/script>');
+document.write('<script src="scripts/app-results-2.js"><\/script>');
+document.write('<script src="scripts/app-results-3.js"><\/script>');
+document.write('<script src="scripts/app-results-4.js"><\/script>');
+document.write('<script src="scripts/app-results-5.js"><\/script>');
+document.write('<script src="scripts/app-results-6.js"><\/script>');
