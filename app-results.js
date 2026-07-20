64cf6a4d8d00e440e9b641b3371cee6009f45473
fchautems <41154950+fchautems@users.noreply.github.com>
2026-07-20 19:10:45 +0200

    Supprime l’ancien chargeur de résultats

--- app-results.js
+++
@@ -1,6 +0,0 @@
-document.write('<script src="scripts/app-results-1.js"><\/script>');
-document.write('<script src="scripts/app-results-2.js"><\/script>');
-document.write('<script src="scripts/app-results-3.js"><\/script>');
-document.write('<script src="scripts/app-results-4.js"><\/script>');
-document.write('<script src="scripts/app-results-5.js"><\/script>');
-document.write('<script src="scripts/app-results-6.js"><\/script>');
